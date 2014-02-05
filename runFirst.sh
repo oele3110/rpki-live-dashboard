86cd4f952d3d3652fad7aba297bda7e4697a01a3
Dennis Lampert <oele3110@hotmail.com>
2014-02-05 13:45:41 +0100

    updated runFirst.sh - added ipv6 rirs

--- runFirst.sh
+++ runFirst.sh
@@ -6,16 +6,27 @@ gunzip -f riswhoisdump.IPv4.gz
 sed '/^%/d' riswhoisdump.IPv4 | sed '/^$/d' | awk '$3 > 4 { print $0 }' | cut -f1,2 | sed -r '/^\{[0-9]*,[0-9]*/d' | sed 's/\t/,/g' | sed 's/{//g' | sed 's/}//g' | awk '{ print $0 ",U,,,4"}' | uniq > riswhoisdump.IPv4.new
 mv riswhoisdump.IPv4.new riswhoisdump.IPv4
 
+
 # get basic IPv6 riswhoisdump
 wget -N http://www.ris.ripe.net/dumps/riswhoisdump.IPv6.gz &> /dev/null
 gunzip -f riswhoisdump.IPv6.gz
 sed '/^%/d' riswhoisdump.IPv6 | sed '/^$/d' | awk '$3 > 4 { print $0 }' | cut -f1,2 | sed -r '/^\{[0-9]*,[0-9]*/d' | sed 's/\t/,/g' | sed 's/{//g' | sed 's/}//g' | awk '{ print $0 ",U,,,6"}' | uniq > riswhoisdump.IPv6.new
 mv riswhoisdump.IPv6.new riswhoisdump.IPv6
 
-# get rir data
+
+# get rir data ipv4
 wget -N http://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.txt
 grep -Eo "whois.*.net" ipv4-address-space.txt | cut -d '.' -f2 > row2
 grep whois.*.net ipv4-address-space.txt | grep -oE "[0-9]{3}/8" | cut -d '/' -f1 | sed -E 's/^0*//' > row1
 paste --delimiter=' ' row1 row2 > rirs
 rm row1 row2
 rm ipv4-address-space.txt
+
+
+# get rir data ipv6
+wget -N http://www.iana.org/assignments/ipv6-unicast-address-assignments/ipv6-unicast-address-assignments.txt
+grep -Eo "whois.*.net" ipv6-unicast-address-assignments.txt | cut -d '.' -f2 > row2
+grep whois.*.net ipv6-unicast-address-assignments.txt | awk '{print $1}' > row1
+paste --delimiter=' ' row1 row2 > rirs-ipv6
+rm row1 row2
+rm ipv6-unicast-address-assignments.txt
